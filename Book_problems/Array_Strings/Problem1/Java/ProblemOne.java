42ffab2d869f70ab03e7eb7bda3eda0007392f1e
StoneVerve <dimavita@ciencias.unam.mx>
2021-06-02 03:15:28 -0500

    Problem1 Java
    Implementation of the first problem form de Array and strings section using Java

---
+++ Book_problems/Array_Strings/Problem1/Java/ProblemOne.java
@@ -0,0 +1,32 @@
+Public Class ProblemOne {
+	
+	// The string is in ASCII
+	public static void main(String[] args) {
+		if(args.length < 1) {
+			System.out.println("Invalid input, please enter a String in ASCII format");
+			return;
+		}
+		if(isUnique(args[0]))
+			System.out.println("The String contains only unique characters");
+		else
+			System.out.println("The String contains at least on duplicate");
+	}
+	
+	
+	private boolean isUnique(String word) {
+		boolean[] characters = boolean[256];
+		int wordLength = word.length();
+		int asciiValue;
+		for(int i = 0; i < wordLength; i++) {
+			asciiValue = (int)word.charAt(i);
+			if (characters[asciiValue])
+				return false;
+			else
+				characters[asciiValue] = true;
+		}
+		return true;
+	}
+	
+	
+
+}
